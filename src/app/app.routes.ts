43b85c2776067cf63ed308dc4010bca933534485
Hugo <hhndez@gmail.com>
2024-12-03 16:08:14 +0000

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserPageComponent } from './users/user-page/user-page.component';
+import { AccessBuildingLogsComponent } from './access-building-logs/access-building-logs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "", component : HomeComponent
+    },
+    {
+        path: "users", component: UserPageComponent
+    }, 
+    {
+        path: "emergency", component: AccessBuildingLogsComponent
+    }
+];

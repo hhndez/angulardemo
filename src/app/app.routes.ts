0394b5e862a27b66d7be11d35cc0e8ba2e9f2cc3
Hugo <hhndez@gmail.com>
2024-12-03 16:17:32 +0000

    Page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { UserPageComponent } from './users/user-page/user-page.component';
 import { AccessBuildingLogsComponent } from './access-building-logs/access-building-logs.component';
 import { AccessRecordComponent } from './access-record/access-record.component';
+import { PACKAGE_ROOT_URL } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +18,8 @@ export const routes: Routes = [
     },
     {
         path: "access", component: AccessRecordComponent
+    }, 
+    {
+        path: "**", component: PageNotFoundComponent
     }
 ];
